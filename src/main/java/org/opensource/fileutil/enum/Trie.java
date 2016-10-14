9be82ee46e4f949db058d43fb6038739d1a86de8
swapanshaw <swapan.1.shaw@gmail.com>
2016-10-14 08:58:57 -0400

    Changes trie logic for  insertion

--- src/main/java/org/opensource/fileutil/enum/Trie.java
+++ src/main/java/org/opensource/fileutil/enum/Trie.java
@@ -2,70 +2,168 @@ package org.opensource.fileutil.enums;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
+import java.util.PriorityQueue;
 
 /**
- * Tri data structure is used to store the file content and that will help to search the text
+ * Trie data structure is used to store the file content and that will help to
+ * search the text
  * 
  * @author sshaw
  *
  */
 public class Trie {
-    private TrieNode root;
+	private TrieNode root;
 
-    public Trie() {
-        this.root = new TrieNode();
-    }
+	public Trie() {
+		this.root = new TrieNode();
+	}
 
-    public void insert(String word) {
-        TrieNode temp = root;
-        final int wc = word.length();
-        for (int i = 0; i < wc; i++) {
-            Character c = word.charAt(i);
-            if (temp.children.get(c) == null) {
-                if (i != wc - 1) {
-                    temp.children.put(c, new TrieNode());
-                } else {
-                    temp.children.put(c, new TrieNode(true));
-                }
-            }
+	public void insert(String word) {
+		TrieNode current = root;
+		final int wc = word.length();
+		for (int i = 0; i < wc; i++) {
+			Character c = word.charAt(i);
+			TrieNode node = current.children.get(c);
+			if (node == null) {
+				node = new TrieNode();
+				current.children.put(c, node);
+			}
+			current = node;
+		}
+		current.setEndOfLine(Boolean.TRUE);
+		current.setFrequncy(current.getFrequncy() == null ? 1 : current.getFrequncy() + 1);
+	}
 
-        }
+	public void insert(String word, PriorityQueue<MinHeapNode> priorityQueue) {
+		TrieNode current = root;
+		final int wc = word.length();
+		for (int i = 0; i < wc; i++) {
+			Character c = word.charAt(i);
+			TrieNode node = current.children.get(c);
+			if (node == null) {
+				node = new TrieNode();
+				current.children.put(c, node);
+			}
+			current = node;
+		}
+		current.setEndOfLine(Boolean.TRUE);
+		current.setFrequncy(current.getFrequncy() + 1);
+		insertIntoPriorityQueue(word, current, priorityQueue);
+	}
 
-    }
+	private void insertIntoPriorityQueue(String word, TrieNode root, PriorityQueue<MinHeapNode> priorityQueue) {
+		MinHeapNode node = null;
+		priorityQueue.add(new MinHeapNode(root, word, root.getFrequncy()));
+	}
 
-    public boolean find(String str) {
-        TrieNode temp = root;
-        Map<Character, TrieNode> triNode = null;
-        for (int i = 0; i < str.length(); i++) {
-            triNode = temp.children;
-            Set<Character> charSet = triNode.keySet();
-            if (!charSet.contains(str.charAt(i))) {
-                return false;
-            }
-        }
+	public boolean find(String str) {
+		TrieNode temp = root;
+		Map<Character, TrieNode> triNode = null;
+		boolean flag = true;
+		for (int i = 0; i < str.length(); i++) {
+			triNode = temp.children;
+			TrieNode node = triNode.get(str.charAt(i));;
+			if(node == null) {
+				flag = false;
+				break;
+			}
+			temp = node;
+		}
 
-        return true;
-    }
+		return flag;
+	}
 
-    /**
-     * TriNode contains Map<Character, TrieNode>
-     * 
-     * @author sshaw
-     *
-     */
-    private class TrieNode {
-        private Map<Character, TrieNode> children;
-        private Boolean endOfLine;
+	/**
+	 * TriNode contains Map<Character, TrieNode>
+	 * 
+	 * @author sshaw
+	 *
+	 */
+	@SuppressWarnings("unused")
+	private class TrieNode {
+		private Map<Character, TrieNode> children;
+		private Boolean endOfLine;
+		private Integer frequncy;
+		private Integer linkToNode;
 
-        public TrieNode() {
-            this.children = new HashMap<>();
-            this.endOfLine = false;
-        }
+		public TrieNode() {
+			this.children = new HashMap<>();
+			this.endOfLine = Boolean.FALSE;
+		}
 
-        public TrieNode(Boolean endOfLine) {
-            this.children = new HashMap<>();
-            this.endOfLine = endOfLine;
-        }
-    }
+		public Map<Character, TrieNode> getChildren() {
+			return children;
+		}
+
+		public void setChildren(Map<Character, TrieNode> children) {
+			this.children = children;
+		}
+
+		public Boolean getEndOfLine() {
+			return endOfLine;
+		}
+
+		public void setEndOfLine(Boolean endOfLine) {
+			this.endOfLine = endOfLine;
+		}
+
+		public Integer getFrequncy() {
+			return frequncy;
+		}
+
+		public void setFrequncy(Integer frequncy) {
+			this.frequncy = frequncy;
+		}
+
+		public Integer getLinkToNode() {
+			return linkToNode;
+		}
+
+		public void setLinkToNode(Integer linkToNode) {
+			this.linkToNode = linkToNode;
+		}
+
+	}
+
+	/**
+	 * 
+	 */
+	@SuppressWarnings("unused")
+	public class MinHeapNode {
+		private TrieNode root; // leave node of the trie
+		private String world;
+		private Integer frequency;
+
+		public MinHeapNode(TrieNode root, String word, Integer frequency) {
+			super();
+			this.root = root;
+			this.world = word;
+			this.frequency = frequency;
+		}
+
+		public TrieNode getRoot() {
+			return root;
+		}
+
+		public void setRoot(TrieNode root) {
+			this.root = root;
+		}
+
+		public String getWorld() {
+			return world;
+		}
+
+		public void setWorld(String world) {
+			this.world = world;
+		}
+
+		public Integer getFrequency() {
+			return frequency;
+		}
+
+		public void setFrequency(Integer frequency) {
+			this.frequency = frequency;
+		}
+
+	}
 }
